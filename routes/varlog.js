c0d104157d7f12e3a6f15cceb19bb22396e8d819
Philip Duncan <poduncan@uwaterloo.ca>
2023-07-14 12:58:59 -0400

    logfiles work with 10 lines

--- routes/varlog.js
+++ routes/varlog.js
@@ -1,49 +1,100 @@
 var express = require('express');
-var fs = require('fs')
+var fs = require('fs/promises');
 
 var router = express.Router();
 
-/* GET home page. */
-router.get('/*', (req, res, next) => {
 
-  const resourcePrefix = '/varlog'
-  var reqpath = resourcePrefix + req.path;
-  var fsPath = '/var/log' + req.path;
+class CircularQueueNonBlocking {
+  constructor(capacity) {
+    this.capacity = capacity;
+    this.storage = Array(capacity);
+    this.head = 0;
+    this.tail = 0;
+    this.size = 0;
+  }
+
+  enqueue(elem) {
+    this.storage[this.tail] = elem;
+    this.tail = (this.tail + 1) % this.capacity;
+
+    if (this.capacity == this.size) {
+      this.head = (this.head + 1) % this.capacity;
+    } else {
+      this.size++;
+    }
+  }
+
+  dequeue() {
+    if (this.size == 0) {
+        throw new Error("circular queue is empty");
+    }
+
+    let elem = this.storage[this.head];
+    this.head = (this.head + 1) % this.capacity;
+    this.size--;
+
+    return elem;
+  }
+
+}
+
+
+/* GET home page. */
+router.get('/*', async (req, res, next) => {
+
+  const resourcePrefix = '/varlog';
+  let reqpathfull = resourcePrefix + req.path;
+  let fsPath = '/var/log' + req.path;
 
   try {
-    var stats = fs.lstatSync(fsPath);
-    
+
+    let stats = await fs.lstat(fsPath);
+
     if (stats.isDirectory()) {
-        var files = fs.readdirSync(fsPath, { withFileTypes: true });
-
-        var logfiles = files.filter(file => file.isFile() && file.name.endsWith('log'));
-        var subdirs = files.filter(file => file.isDirectory());
-
-        res.json({
-          logfiles: logfiles.map(logfile => ({ resource: reqpath + logfile.name })),
-          subdirs: subdirs.map(subdir => ({ resource: reqpath + subdir.name }))
-        });
-        return;
+      let files = await fs.readdir(fsPath, { withFileTypes: true });
+    
+      let logfiles = files.filter(file => file.isFile() && file.name.endsWith('log'));
+      let subdirs = files.filter(file => file.isDirectory());
+    
+      res.json({
+        logfiles: logfiles.map(logfile => ({ resource: reqpathfull + logfile.name })),
+        subdirs: subdirs.map(subdir => ({ resource: reqpathfull + subdir.name }))
+      });
+      return;
     }
 
     if (stats.isFile()) {
-        res.json(stats);
-        return;
+
+      const filehandle = await fs.open(fsPath);
+
+      let circularqueue = new CircularQueueNonBlocking(10);
+      for await (const line of filehandle.readLines()) {
+        circularqueue.enqueue(line);
+      }
+
+      let lines = Array(circularqueue.size);
+      for (let i = 0; i < lines.length; i++) {
+        lines[i] = circularqueue.dequeue();
+      }
+
+      res.json({lines: lines});
+      
+      return
     }
 
     res.status('404');
     res.json({});
-
+  
   } catch (err) {
 
     if (err.code == 'ENOENT') {
-      res.status('404');
-    } else {
-      res.status('500');
-    }
-    
-    res.json(err);
-  }
+        res.status('404');
+      } else {
+        res.status('500');
+      }
+      
+      res.json(err);
+  };
 
 });
 
